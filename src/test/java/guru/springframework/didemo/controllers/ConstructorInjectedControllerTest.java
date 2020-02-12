8baf354eeaadfd65e80ca56dd7e19eeeee624a88
Yersultan Medetov <y.medetov@gmail.com>
2020-02-12 23:34:44 +0600

    initial commit

---
+++ src/test/java/guru/springframework/didemo/controllers/ConstructorInjectedControllerTest.java
@@ -0,0 +1,22 @@
+package guru.springframework.didemo.controllers;
+
+import guru.springframework.didemo.services.GreetingServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ConstructorInjectedControllerTest {
+
+    private ConstructorInjectedController constructorInjectedController;
+
+    @BeforeEach
+    public void setUp() throws Exception {
+        this.constructorInjectedController = new ConstructorInjectedController(new GreetingServiceImpl());
+    }
+
+    @Test
+    public void testGreeting() throws Exception {
+        assertEquals(GreetingServiceImpl.HELLO_GURUS, constructorInjectedController.sayHello());
+    }
+}
---
+++ src/test/java/guru/springframework/didemo/controllers/PropertyInjectedControllerTest.java
@@ -0,0 +1,24 @@
+package guru.springframework.didemo.controllers;
+
+import guru.springframework.didemo.services.GreetingServiceImpl;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class PropertyInjectedControllerTest {
+
+    private PropertyInjectedController propertyInjectedController;
+
+    @BeforeEach
+    public void setUp() throws Exception {
+        this.propertyInjectedController = new PropertyInjectedController();
+        this.propertyInjectedController.greetingServiceImpl = new GreetingServiceImpl();
+    }
+
+    @Test
+    public void testGreeting() throws Exception {
+        assertEquals(GreetingServiceImpl.HELLO_GURUS, propertyInjectedController.sayHello());
+    }
+}
